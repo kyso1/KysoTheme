ebca8fec6bea57c2076e1c0e3ddc86b89ddc0e5c
kyso1 <antropofagia9@gmail.com>
2024-04-24 15:00:53 -0300

    Sobe atualização index

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+ * @name KysoThemev0.0.1
+ * @author Kyso (agonised)
+ * @description Kyso theme for League of Legends (Pengu Loader)
+ * @link https://github.com/kyso1
+*/
+
 function addCss(filename) {
   const style = document.createElement('link')
   style.href = filename
@@ -5,10 +12,15 @@ function addCss(filename) {
   style.rel = 'stylesheet'
   document.body.append(style)
 }
+import "./main-theme/theme.css"
 
 window.onload = () => {
   // External CDN for css hosting.
+  //addCss("https://rawcdn.githack.com/kyso1/KysoTheme/83a4332c28468a45130904e478849a9deb51714a/main-theme/theme.css");
+  //console.log("Theme loaded successfully! Enjoy ;)");
+
+  // Loads the css from local filesystem
   
-  addCss("https://rawcdn.githack.com/kyso1/KysoTheme/83a4332c28468a45130904e478849a9deb51714a/main-theme/theme.css");
-  console.log("Theme loaded successfully! Enjoy ;)");
+  //const cssPath = "./main-theme/theme.css";
+  addCss(theme);
 }
