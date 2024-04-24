036cb96a43ae8be37ca872a434c05de0ebe6bfae
Kyso <128049220+kyso1@users.noreply.github.com>
2024-04-24 15:02:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ import "./main-theme/theme.css"
 
 window.onload = () => {
   // External CDN for css hosting.
-  //addCss("https://rawcdn.githack.com/kyso1/KysoTheme/83a4332c28468a45130904e478849a9deb51714a/main-theme/theme.css");
-  //console.log("Theme loaded successfully! Enjoy ;)");
+  addCss("https://rawcdn.githack.com/kyso1/KysoTheme/83a4332c28468a45130904e478849a9deb51714a/main-theme/theme.css");
+  console.log("Theme loaded successfully! Enjoy ;)");
 
   // Loads the css from local filesystem
   
   //const cssPath = "./main-theme/theme.css";
-  addCss(theme);
+  //addCss(theme);
 }
